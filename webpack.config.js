8517cc5269e76b3a54f57314e5b078282b3eb8ac
Zendau <cowalyow2012@gmail.com>
2021-01-02 19:27:50 +0300

    test

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const isDev = process.env.NODE_ENV  === "development"
+const isProd = !isDev
+
+console.log("isDev "+isDev)
+console.log("isProd "+isProd)
+
+const get_file_name = env => isDev ? `bundle.${env}` : `bundle[hash].${env}`
+
+module.exports = {
+
+    mode: 'development',
+    entry: '/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: get_file_name("js")
+    },
+    devtool: isDev ? 'source-map' : false,
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            "@core": path.resolve(__dirname, "src/core")
+        }
+    },
+    devServer: {
+        hot: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            minify: {
+                collapseWhitespace: isProd,
+                removeComments: isProd,
+            }
+        }),
+        new MiniCssExtractPlugin({
+             filename: get_file_name("css")
+        }),
+        new ESLintPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    }
+}
